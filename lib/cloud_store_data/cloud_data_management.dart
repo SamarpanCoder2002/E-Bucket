5c9d1da24ae273e9c6a4a513abc48ad952938ddd
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-07-22 06:06:15 +0530

    Sell On E-Bucket Completed Except Image Uploading

--- lib/cloud_store_data/cloud_data_management.dart
+++ lib/cloud_store_data/cloud_data_management.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -260,21 +259,3 @@ class CloudDataStore {
     }
   }
 }
-=======
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class CloudDataStore{
-  final String _consumerPath = 'consumers';
-  final String _sellerPath = 'sellers';
-
-  Future<void> dataStoreForConsumers(String email) async{
-    try{
-      await FirebaseFirestore.instance.doc('$_consumerPath/$email').set({
-        'email':'$email',
-      });
-    }catch(e){
-      print('Data Store For Consumers Error: ${e.toString()}');
-    }
-  }
-}
->>>>>>> 4cb0df3213bd8c768630bf00cac93fd2ff4f9726
